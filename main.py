db518a6cc9063a5ab2594ffa955e8ce2dec91296
Craig Bourne <craigbourne15@gmail.com>
2024-06-23 23:39:25 +0100

    add contact function

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 contacts = {}
 
+def add_contact():
+    while True:
+        # collect name and number
+        name = input("Contact name: ")
+        phone = input("Phone number: ")
+
+        # contacts key is name and value is phone number
+        contacts[name] = phone
+        print(contacts)
+
+    # need to break out of loop asking for new contact
+
 # ask the user what they want to do
 def user_choice():
     while True:
         choice = int(input("\n What would you like to do? \n[0] add contact \n[1] view contact \n[2] edit a contact \n[3] delete a contact \n"))
 
         if choice in (0,1,2,3):
-            if choice == 0:
-                while True:
-                    name = input("Contact name: ")
-                    phone = input("Phone number: ")
-                    # contacts key is name and value is phone number
-                    contacts[name] = phone
-                    print(contacts)
-
-                    # need to break out of askinf for new contact
-
+            if choice == 0: add_contact()
             elif choice == 1:
                 # view contact
                 print("view contact")
