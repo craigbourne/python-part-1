f35729afdd61b9c34b3b99cf20e58a95ce4d1ebd
Craig Bourne <craigbourne15@gmail.com>
2024-06-23 22:57:56 +0100

    created user choice function. Need a way to exit program

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 contacts = {}
 
 # ask the user what they want to do
-# maybe put this in a function
-while True:
-    choice = int(input("\n What would you like to do? \n[0] add contact \n[1] view contact \n[2] edit a contact \n[3] delete a contact"))
+def user_choice():
+    while True:
+        choice = int(input("\n What would you like to do? \n[0] add contact \n[1] view contact \n[2] edit a contact \n[3] delete a contact \n"))
 
-    if choice in (0,1,2,3):
-        if choice == 0:
-            #add contact
-            print("add contact")
-        elif choice == 1:
-            # view contact
-            print("view contact")
-        elif choice == 2:
-            # edit contact 
-            print("edit contact ")
+        if choice in (0,1,2,3):
+            if choice == 0:
+                # add contact
+                print("add contact")
+            elif choice == 1:
+                # view contact
+                print("view contact")
+            elif choice == 2:
+                # edit contact
+                print("edit contact ")
+            else:
+                # delete contact
+                print("delete contact ")
         else:
-            # delete contact 
-            print("delete contact ")
-    else:
-        print("Invalid Choice. Please Try Again.")
+            print("Invalid Choice. Please Try Again.")
+
+user_choice()
