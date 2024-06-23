1516ab6da90a14a5d2cd9632bc31cb5315b73683
Craig Bourne <craigbourne15@gmail.com>
2024-06-23 22:51:36 +0100

    ask user what the want to do

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 contacts = {}
 
+# ask the user what they want to do
+# maybe put this in a function
+while True:
+    choice = int(input("\n What would you like to do? \n[0] add contact \n[1] view contact \n[2] edit a contact \n[3] delete a contact"))
+
+    if choice in (0,1,2,3):
+        if choice == 0:
+            #add contact
+            print("add contact")
+        elif choice == 1:
+            # view contact
+            print("view contact")
+        elif choice == 2:
+            # edit contact 
+            print("edit contact ")
+        else:
+            # delete contact 
+            print("delete contact ")
+    else:
+        print("Invalid Choice. Please Try Again.")
