e827aafaff32a9ff770ee39fcc50c3afba39fbf6
Craig Bourne <craigbourne15@gmail.com>
2024-06-24 10:50:04 +0100

    code to break out of add contact loop

--- main.py
+++ main.py
@@ -11,20 +11,24 @@ def add_contact():
         print(contacts)
 
     # need to break out of loop asking for new contact
+        if input("Would you like to add another contact? (y/n) : ").lower() != 'y':
+            break
 
 # ask the user what they want to do
 def user_choice():
     while True:
-        choice = int(input("\n What would you like to do? \n[0] add contact \n[1] view contact \n[2] edit a contact \n[3] delete a contact \n"))
+        choice = int(input("\n What would you like to do? \n[0] add contact \n[1] search contact \n[2] delete contact \n"))
 
-        if choice in (0,1,2,3):
+        if choice in (0,1,2):
             if choice == 0: add_contact()
             elif choice == 1:
-                # view contact
-                print("view contact")
-            elif choice == 2:
-                # edit contact
-                print("edit contact ")
+                # search contact
+                print("search contact ")
+                
+
+
+
+
             else:
                 # delete contact
                 print("delete contact ")
